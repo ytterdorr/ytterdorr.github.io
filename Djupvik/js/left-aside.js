2099ff9371aea4c17026a326a4cb119c4dab9681
Tore Haglund <tore.haglund@gmail.com>
2020-08-15 18:40:24 +0200

    Did this fix routing?

--- Djupvik/js/left-aside.js
+++ Djupvik/js/left-aside.js
@@ -1,16 +1,23 @@
 let leftSide = document.querySelector(".left-side");
 console.log(leftSide);
 
+let relPath;
+if (window.location.origin == "https://ytterdorr.github.io") {
+  relPath = "/Djupvik";
+} else {
+  relPath = "";
+}
+
 leftSide.innerHTML = `
 <nav>
   <ul>
-    <a href="/index.html"><li>Hem</li></a>
-    <a href="/html/gasthamn.html"><li>Gästhamn</li></a>
-    <a href="/html/kontakt.html"><li>Kontakt</li></a>
-    <a href="/html/turism.html"><li>Turism</li></a>
-    <a href="/html/historia.html"><li>Historia</li> </a>
-    <a href="/html/medlem.html"><li>Medlem</li> </a>
-    <a href="/html/english.html"><li>English</li></a>
+    <a href="${relPath}/index.html"><li>Hem</li></a>
+    <a href="${relPath}/html/gasthamn.html"><li>Gästhamn</li></a>
+    <a href="${relPath}/html/kontakt.html"><li>Kontakt</li></a>
+    <a href="${relPath}/html/turism.html"><li>Turism</li></a>
+    <a href="${relPath}/html/historia.html"><li>Historia</li> </a>
+    <a href="${relPath}/html/medlem.html"><li>Medlem</li> </a>
+    <a href="${relPath}/html/english.html"><li>English</li></a>
   </ul>
 </nav>
 
